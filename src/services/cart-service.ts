cf736d1d5f105b556e0cdfbee2e6e7c2c6968298
Gustavo Moreno <gmorenosmail@gmail.com>
2024-03-30 01:11:23 -0300

    Project Restore Place Order

---
+++ src/services/cart-service.ts
@@ -0,0 +1,46 @@
+import { OrderDTO, OrderItemDTO } from "../models/order";
+import * as cartRepositoy from '../localstorage/cart-repository';
+import { ProductDTO } from "../models/product";
+
+export function saveCart(cart: OrderDTO) {
+    cartRepositoy.save(cart);
+}
+
+export function getCart() : OrderDTO {
+    return cartRepositoy.get();
+}
+
+export function addProduct(product : ProductDTO) {
+    const cart = cartRepositoy.get();
+    const item = cart.items.find(x => x.productId === product.id);
+    if (!item) {
+        const newItem = new OrderItemDTO(product.id, 1, product.name, product.price, product.imgUrl);
+        cart.items.push(newItem);
+        cartRepositoy.save(cart);
+    }
+}
+
+export function clearCart() {
+    cartRepositoy.clear();
+}
+
+export function increaseItem(productId: number) {
+    const cart = cartRepositoy.get();
+    const item = cart.items.find(x => x.productId === productId);
+    if (item) {
+        item.quantity++
+        cartRepositoy.save(cart);        
+    }    
+}
+
+export function decreaseItem(productId: number) {
+    const cart = cartRepositoy.get();
+    const item = cart.items.find(x => x.productId === productId);
+    if (item) {
+        item.quantity--
+        if (item.quantity < 1) {
+            cart.items = cart.items.filter(x => x.productId !== productId);
+        }
+        cartRepositoy.save(cart);        
+    }    
+}
