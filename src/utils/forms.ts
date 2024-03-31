a1708630924276adeb254e01ba2f13b1105630ec
Gustavo Moreno <gmorenosmail@gmail.com>
2024-03-31 07:12:10 -0300

    forms toValues

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -1,5 +1,14 @@
+/* eslint-disable no-var */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 export function update(inputs: any, name: string, newValue: any) {
   return { ...inputs, [name]: { ...inputs[name], value: newValue } };
 }
+
+export function toValues(inputs: any) {
+    const data: any = {};
+    for (var name in inputs) {
+      data[name] = inputs[name].value;
+    }
+    return data;
+  }
