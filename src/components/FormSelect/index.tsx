b74791d8732b8ea9d7f5685efbbc058758356180
Gustavo Moreno <gmorenosmail@gmail.com>
2024-03-31 11:27:53 -0300

    React Select integration

---
+++ src/components/FormSelect/index.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import Select from "react-select";
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export default function FormSelect(props: any) {
+    const {
+        className,
+        validation,
+        invalid = "false",
+        dirty = "false",
+        onTurnDirty,
+        ...selectProps
+    } = props;
+
+    function handleBlur() {
+        onTurnDirty(props.name);
+    }
+    return (
+        <div
+            className={className}
+            data-invalid={invalid}
+            data-dirty={dirty}
+        >
+            <Select
+                {...selectProps}
+                onBlur={handleBlur}
+            />
+        </div>
+    );
+}
--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -7,8 +7,8 @@ import * as forms from '../../../utils/forms';
 import * as productService from '../../../services/product-service'
 import * as categoryService from '../../../services/category-service';
 import FormTextArea from '../../../components/FormTextArea/indext';
-import Select from 'react-select';
 import { CategoryDTO } from '../../../models/category';
+import FormSelect from '../../../components/FormSelect';
 
 export default function ProductForm() {
 
@@ -59,6 +59,16 @@ export default function ProductForm() {
           return /^.{10,}$/.test(value);
       },
       message: "A descrição com minimo de 10 caracteres!"
+    },
+    categories: {
+      value: [],
+      id: "categories",
+      name: "categories",
+      placeholder: "Categorias",
+      validation: function (value: CategoryDTO[]) {
+          return value.length > 0;
+      },
+      message: "Escolha ao menos uma categoria"
     }
 
   });
@@ -123,12 +133,20 @@ export default function ProductForm() {
                     />
                 </div>
                 <div>
-                    <Select 
+                    <FormSelect 
+                    {...formData.categories}
+                    className="dsc-form-control" 
                     options={categories}
+                    onChange={(obj: any) => {
+                      const newFormData = forms.updateAndValidate(formData, "categories", obj);
+                      setFormData(newFormData);
+                    }}
+                    onTurnDirty={handleTurnDirty}
                     isMulti 
-                    getOptionLabel={(obj) => obj.name}
-                    getOptionValue={(obj) => String(obj.id)}
+                    getOptionLabel={(obj: any) => obj.name}
+                    getOptionValue={(obj: any) => String(obj.id)}
                     />
+                    <div className="dsc-form-error">{formData.categories.message}</div>
                 </div>
                 <div>
                     <FormTextArea
