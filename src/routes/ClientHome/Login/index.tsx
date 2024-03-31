ff147a8bdd1c8e2246df8f136651e8d4c53e4e52
Gustavo Moreno <gmorenosmail@gmail.com>
2024-03-31 17:44:08 -0300

    Login form global message

--- src/routes/ClientHome/Login/index.tsx
+++ src/routes/ClientHome/Login/index.tsx
@@ -12,6 +12,8 @@ export default function Login() {
 
   const navigate = useNavigate();
 
+  const [submitResponseFail, setSubmitResponseFail] = useState(false);
+
   const [formData, setFormData] = useState<any>({
     username: {
       value: "",
@@ -35,6 +37,15 @@ export default function Login() {
 
   function handleSubmit(event: any) {
     event.preventDefault();
+    
+    setSubmitResponseFail(false);
+
+    const formDataValidated = forms.dirtyAndValidateAll(formData);
+    if (forms.hasAnyInvalid(formDataValidated)) {
+      setFormData(formDataValidated);
+      return;
+    }
+
     authService
       .loginRequest(forms.toValues(formData))
       .then((response) => {
@@ -42,8 +53,8 @@ export default function Login() {
         setContextTokenPayload(authService.getAccessTokenPayload());
         navigate("/cart");
       })
-      .catch((error) => {
-        console.log("Erro no login", error);
+      .catch(() => {
+        setSubmitResponseFail(true);
       });
   }
 
@@ -81,6 +92,14 @@ export default function Login() {
                 />
               </div>
             </div>
+
+            {
+              submitResponseFail &&
+              <div className="dsc-form-global-error">
+              Usuário ou senha inválidos
+              </div>
+            }
+
             <div className="dsc-login-form-buttons dsc-mt20">
               <button type="submit" className="dsc-btn dsc-btn-blue">
                 Entrar
