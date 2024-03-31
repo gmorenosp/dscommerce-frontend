f21b8787501d26bc219d81c04a850aadd13955d1
Gustavo Moreno <gmorenosmail@gmail.com>
2024-03-31 07:04:22 -0300

    forms update

---
+++ src/utils/forms.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export function update(inputs: any, name: string, newValue: any) {
+  return { ...inputs, [name]: { ...inputs[name], value: newValue } };
+}
