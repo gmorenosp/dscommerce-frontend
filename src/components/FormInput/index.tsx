d7aade34f3e845432a4a222bed0f204489b7918b
Gustavo Moreno <gmorenosmail@gmail.com>
2024-03-31 06:56:20 -0300

    Custom FormInput

---
+++ src/components/FormInput/index.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export default function FormInput(props: any) {
+
+    const { validation, ...inputProps } = props;
+    /*
+    const {
+        validation,
+        invalid = "false",
+        dirty = "false",
+        onTurnDirty,
+        ...inputProps
+    } = props;
+
+    function handleBlur() {
+        onTurnDirty(props.name);
+    }
+*/
+
+return (
+    <input {...inputProps} />
+    );
+/*
+    return (
+        <input
+            {...inputProps}
+            onBlur={handleBlur}
+            data-invalid={invalid}
+            data-dirty={dirty}
+        />
+    ); */
+}
